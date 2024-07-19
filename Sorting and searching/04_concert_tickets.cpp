0f7efa674341ce4e98db95e6f1c9b3cad882f0fc
Soumo275 <soumostudy27@gmail.com>
2024-07-20 00:40:28 +0530

    new

---
+++ Sorting and searching/04_concert_tickets.cpp
@@ -0,0 +1,34 @@
+#include "bits/stdc++.h"
+using namespace std;
+int main() {
+    
+    int n, m;
+    cin >> n >> m;
+    map<int, int> tickets;
+    vector<int> customer(m);
+    for (int i = 0; i < n; i++){
+        int a;
+        cin >> a;
+        tickets[a]++;
+    }
+    for (int i = 0; i < m; i++) cin >> customer[i];
+    
+    for(int i=0;i<m;i++){
+
+        auto it = tickets.upper_bound(customer[i]);
+        if (it == tickets.begin()) {
+            cout << -1 << "\n";
+        }
+        else {
+            it--;
+            cout << it->first << "\n";
+            it->second -= 1;
+            if (it->second == 0)
+                tickets.erase(it);
+        }
+
+
+    }
+
+    return 0;
+}
